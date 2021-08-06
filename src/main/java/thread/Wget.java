f0e63f7b1f95bb9680decb200aa5d665a7546ecc
Илья <qq.ferveks@gmail.com>
2021-08-06 14:54:31 +0300

    4. Скачивание файла с ограничением. [#144271]

--- src/main/java/thread/Wget.java
+++ src/main/java/thread/Wget.java
@@ -1,9 +1,49 @@
 package thread;
 
+import java.io.*;
+import java.net.URI;
+import java.net.URL;
 import java.sql.SQLOutput;
 
-public class Wget {
-    public static void main(String[] args) {
+public class Wget implements Runnable{
+    private final String url;
+    private final int speed;
+
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream fos = new FileOutputStream("pom_tpm.xml")) {
+            byte[] dataBuffer = new byte[1024];
+            int bytesRead;
+            long timeStartLoading = System.currentTimeMillis();
+            long timeLoading;
+            while ((bytesRead = in.read(dataBuffer, 0, dataBuffer.length)) != -1) {
+                fos.write(dataBuffer, 0, bytesRead);
+                timeLoading = System.currentTimeMillis() - timeStartLoading;
+                if (timeLoading < speed) {
+                    Thread.sleep(speed - timeLoading);
+                }
+                timeStartLoading = System.currentTimeMillis();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        String url = "https://github.com/ferveks3509/job4j_threads/blob/master/pom.xml";
+        int speed = 1000;
+        Thread wGet = new Thread(new Wget(url, speed));
+        wGet.start();
+        wGet.join();
+        /*
         Thread thread = new Thread(
                 () -> {
                     try {
@@ -17,5 +57,7 @@ public class Wget {
                 }
         );
         thread.start();
+
+         */
     }
 }
