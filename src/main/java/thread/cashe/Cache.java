1d355d20514f442cd9eb99803f1a298f67f36a6d
Илья <qq.ferveks@gmail.com>
2021-08-13 12:49:32 +0300

    0. CAS - операции [#6859]

---
+++ src/main/java/thread/cashe/Cache.java
@@ -0,0 +1,23 @@
+package thread.cashe;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Cache {
+    private final Map<Integer, Base> memory = new ConcurrentHashMap<>();
+
+    public boolean add(Base model) {
+        return memory.putIfAbsent(model.getId(), model) == null;
+    }
+
+    public boolean update(Base model) {
+        Base stored = memory.get(model.getId());
+        if (stored.getVersion() != model.getVersion()) {
+        }
+        return false;
+    }
+
+    public void delete(Base model) {
+        /* TODO impl */
+    }
+}
