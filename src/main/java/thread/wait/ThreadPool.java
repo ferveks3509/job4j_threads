95687ce1c7e5a23a4b03dc77bf6317284a86e74e
Илья <qq.ferveks@gmail.com>
2021-08-14 14:07:06 +0300

    2. Обеспечить остановку потребителя. [#66825 #188713]

---
+++ src/main/java/thread/wait/ThreadPool.java
@@ -0,0 +1,17 @@
+package thread.wait;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ThreadPool {
+    private final List<Thread> threads = new LinkedList<>();
+    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(10);
+
+    public void work(Runnable job) {
+
+    }
+
+    public void shutdown() {
+
+    }
+}
